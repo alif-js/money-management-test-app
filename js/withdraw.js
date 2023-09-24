528ef34a5fd844e8a9739eeedbfdaa47bbd1381f
alif-js <alif.hubspot@gmail.com>
2023-09-24 19:06:34 +0600

    another fix injected

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,17 +1,20 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithDrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdrawAmount = getTextElementValueById('withdraw-total');
-    // if (newWithDrawAmount > previousWithdrawAmount) {
-    //     alert("Insufficient Balance");
-    //     return;
-    // }
-
-    const withdrawTotal = newWithDrawAmount + previousWithdrawAmount;
-    setTextElementValueById('withdraw-total', withdrawTotal)
+    
 
 
 
     const previousBalanceTotal = getTextElementValueById('balance-total');
+
+    if (newWithDrawAmount > previousBalanceTotal) {
+        alert("Insufficient Balance");
+        return;
+    }
+
+    const withdrawTotal = newWithDrawAmount + previousWithdrawAmount;
+    setTextElementValueById('withdraw-total', withdrawTotal)
+
     const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
     setTextElementValueById('balance-total', newBalanceTotal);
 })
