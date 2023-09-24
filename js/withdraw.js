5f501c21f76b50ccb9f590ba5cf55bd32d26d9ff
alif-js <alif.hubspot@gmail.com>
2023-09-24 18:45:41 +0600

    handled insufficient balance withdrawals

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,8 +1,14 @@
-document.getElementById('btn-withdraw').addEventListener('click', function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithDrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdrawAmount = getTextElementValueById('withdraw-total');
-    const withdrawTotal = newWithDrawAmount + previousWithdrawAmount;
-    setTextElementValueById('withdraw-total', withdrawTotal)
+    if (newWithDrawAmount > previousWithdrawAmount) {
+        alert("Insufficient Balance");
+    }
+    else {
+        const withdrawTotal = newWithDrawAmount + previousWithdrawAmount;
+        setTextElementValueById('withdraw-total', withdrawTotal)
+    }
+
 
     const previousBalanceTotal = getTextElementValueById('balance-total');
     const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
